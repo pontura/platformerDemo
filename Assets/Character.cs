8b86b06b32a37813417e332e2769347116d14a75
Agustin Pontura <pontura@gmail.com>
2018-09-22 11:40:32 -0300

    movimiento forzado

--- Assets/Character.cs
+++ Assets/Character.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Character : MonoBehaviour {
 
 	public Rigidbody2D rb;
-	public float moveForce;// = 360f;
+	public float speed;// = 360f;
 	public float maxSpeed;//  = 5f;
 	public float jumpForce;//  = 500f;
 	public float jumpSecondFactor;//  = 250f;
@@ -16,38 +16,19 @@ public class Character : MonoBehaviour {
 
 	float lastTimeOnFloor;
 	void Update () {
+		float h = Input.GetAxis ("Horizontal");
+		rb.velocity = new Vector2(h*speed, rb.velocity.y);
 		grounded = Physics2D.Linecast (transform.position, groundCheck.position, 1 << LayerMask.NameToLayer ("Floor"));
 		if (Time.time > lastTimeOnFloor + 0.2f && grounded)
 			jumps = 0;
 		if (Input.GetButtonDown ("Jump") && grounded == true && jumps == 0) {
 			lastTimeOnFloor = Time.time;
-			jump = true;
+			rb.AddForce (new Vector2(0f, jumpForce));
 			jumps = 1;
 		} else if (Input.GetButtonDown ("Jump") && jumps == 1) {
 			rb.velocity = new Vector2(rb.velocity.x, 0);
-			jump = true;
+			rb.AddForce (new Vector2(0f, jumpForce*jumpSecondFactor));
 			jumps = 2;
-		}
-
-	}
-
-	void FixedUpdate()
-	{
-		float h = Input.GetAxis ("Horizontal");
-		print (h);
-
-		if(h * rb.velocity.x < maxSpeed)
-			rb.AddForce (Vector2.right * h * moveForce);
-
-		if(Mathf.Abs(rb.velocity.x) > maxSpeed)
-			rb.velocity = new Vector2(Mathf.Sign(rb.velocity.x) * maxSpeed, rb.velocity.y);
-
-		if (jump) {
-			if(jumps == 1)
-				rb.AddForce (new Vector2(0f, jumpForce));
-			else
-				rb.AddForce (new Vector2(0f, jumpForce*jumpSecondFactor));
-			jump = false;
 		}
 	}
 }
