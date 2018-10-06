c0c843a5be6f84825027fe5726823de82f2edfbd
Agustin Pontura <pontura@gmail.com>
2018-10-06 12:33:57 -0300

    audios

--- Assets/SoundManager.cs
+++ Assets/SoundManager.cs
@@ -13,9 +13,10 @@ public class SoundManager : MonoBehaviour {
 	void OnSoundFX(string audioName)
 	{
 		AudioSource source = gameObject.AddComponent<AudioSource>();
-		source.clip = Resources.Load(audioName) as AudioClip;
+		AudioClip clip = Resources.Load(audioName) as AudioClip;
+		source.clip = clip;
 		source.pitch = Mathf.Lerp( 0.4f, 1.5f, ((float)Random.Range (1, 10)/10) );
 		source.Play();
-		Destroy(source, 1);
+		Destroy (source, source.clip.length);
 	}
 }
