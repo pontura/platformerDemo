dfcaca2f9129ae56a1ed9cd01a86f7c5d2cce7e1
Agustin Pontura <pontura@gmail.com>
2018-10-06 09:41:31 -0300

    anims

--- Assets/Character.cs
+++ Assets/Character.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Character : MonoBehaviour {
 
+	public Animator anim;
 	public int coins;
 	public Rigidbody2D rb;
 	public float speed;// = 360f;
@@ -15,6 +16,15 @@ public class Character : MonoBehaviour {
 	public Transform groundCheck;
 	public int jumps;
 
+	public states state;
+	public enum states
+	{
+		IDLE,
+		WALKING,
+		JUMP,
+		REACH_FLOOR
+	}
+
 	float lastTimeOnFloor;
 	void Update () {
 		float h = Input.GetAxis ("Horizontal");
@@ -23,12 +33,21 @@ public class Character : MonoBehaviour {
 			transform.localScale = new Vector3 (-1, 1, 1);
 		else if (h > 0)
 			transform.localScale = new Vector3 (1, 1, 1);
+
+		if (h != 0) {
+			Walk ();
+		} else {
+			Idle ();
+		}
 		
 		rb.velocity = new Vector2(h*speed, rb.velocity.y);
 		grounded = Physics2D.Linecast (transform.position, groundCheck.position, 1 << LayerMask.NameToLayer ("Floor"));
-		if (Time.time > lastTimeOnFloor + 0.2f && grounded)
+		if (Time.time > lastTimeOnFloor + 0.2f && grounded) {
+			state = states.REACH_FLOOR;
 			jumps = 0;
+		}
 		if (Input.GetButtonDown ("Jump") && grounded == true && jumps == 0) {
+			Jump ();
 			lastTimeOnFloor = Time.time;
 			rb.AddForce (new Vector2(0f, jumpForce));
 			jumps = 1;
@@ -41,5 +60,29 @@ public class Character : MonoBehaviour {
 	public void Die()
 	{
 		print ("die");
+	}
+	void Walk()
+	{
+		if (state == states.WALKING)
+			return;
+		state = states.WALKING;
+
+		anim.Play ("walk");
+	}
+	void Idle()
+	{
+		if (state == states.IDLE || state == states.JUMP)
+			return;
+		state = states.IDLE;
+
+		anim.Play ("idle");
+	}
+	void Jump()
+	{
+		if (state == states.JUMP)
+			return;
+		state = states.JUMP;
+
+		anim.Play ("jump");
 	}
 }
